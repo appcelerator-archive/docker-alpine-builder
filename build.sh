69f06a8d77010b4331d81cd95858784cf1068a10
freignat <freignat@axway.com>
2016-10-25 14:58:52 +0200

    update comment

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "execute build "$1
+echo "execute build "$1" binary"
 if [[ $1 == "" ]]
 then
      echo "a project name should be specified"
      exit 1
 fi
-
 cd /go/src/github.com/appcelerator/$1
 make install
 cp /go/bin/$1 $1.alpine
